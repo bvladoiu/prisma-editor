b90bf85a54bf831b6a5c839bc25fe636777bd97d
bvladoiu <bogdan.vladoiu.lbs@gmail.com>
2025-05-13 15:27:13 +0300

    Review fixes: Add missing Typography properties to fix compilation errors

--- src/jsMain/kotlin/prisma/editor/css/Typography.kt
+++ src/jsMain/kotlin/prisma/editor/css/Typography.kt
@@ -9,6 +9,22 @@ object Typography {
     // Font families
     val displayFontFamily = "'Poppins', sans-serif"
     val regularFontFamily = "'Roboto Flex', sans-serif"
+    val defaultFontFamily = regularFontFamily
+
+    // Font sizes
+    val fontXs = "12px"
+    val fontSm = "14px"
+    val fontMd = "16px"
+    val fontLg = "20px"
+    val fontXl = "24px"
+
+    // Font weights
+    val fontWeightNormal = "400"
+    val fontWeightBold = "700"
+
+    // Line heights
+    val lineHeightNormal = "1.5"
+    val lineHeightLarge = "1.8"
 
     // Basic body styling
     fun getCssRules(): List<CssRuleDefinition> {
